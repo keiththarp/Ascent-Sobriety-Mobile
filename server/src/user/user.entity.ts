88f1582b2668f1b400d0f785770f6fe8ffb24378
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-12-22 18:23:01 -0500

    server connects to DB creates table

---
+++ server/src/user/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
