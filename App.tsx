ab2fb8ca9ec61878acb14b2d727e27e13ca14b8e
Keith Tharp <keithstharp@gmail.com>
2020-12-31 10:45:16 -0500

    readme and launch screen customizations

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Ascent Sobriety</Text>
+      <Text style={styles.mobile}>Mobile</Text>
+      <View style={styles.hr} />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,5 +20,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  mobile: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  hr: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    width: '100%',
+    marginBottom: 15,
   },
 });
