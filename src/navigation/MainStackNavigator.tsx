455dc445455a034966bd2aa04388f387bf1961cb
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-02-28 18:30:26 -0500

    Add TabNavigator for bottom nav

--- src/navigation/MainStackNavigator.tsx
+++ src/navigation/MainStackNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/HomeScreen';
 import AboutScreen from '../screens/AboutScreen';
@@ -8,8 +9,9 @@ import JournalScreen from '../screens/JournalScreen';
 import ResourceScreen from '../screens/ResourceScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const MainStackNavigator: React.FC = () => {
+export const MainStackNavigator: React.FC = () => {
   const { Navigator, Screen } = Stack;
 
   return (
@@ -40,7 +42,56 @@ const MainStackNavigator: React.FC = () => {
   )
 }
 
-export default MainStackNavigator;
+export const TabNavigator: React.FC = () => {
+  return (
+
+  <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName='Home'
+      tabBarOptions={{
+        activeTintColor: 'grey',
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          title: 'Ascent Mobile',
+          tabBarLabel: 'Home'
+        }}
+        />
+      <Tab.Screen
+        name='Journal'
+        component={JournalScreen}
+        options={{
+          title: 'Journal Screen',
+          tabBarLabel: 'Journal'
+        }}
+      />
+      <Tab.Screen
+        name='Resources'
+        component={ResourceScreen}
+        options={{
+          title: 'Resources Screen',
+          tabBarLabel: 'Resources'
+        }}
+      />
+      <Tab.Screen
+        name='About'
+        component={AboutScreen}
+        options={{
+          title: 'About Screen',
+          tabBarLabel: 'About'
+        }}
+      />
+
+    </Tab.Navigator>
+  </NavigationContainer>
+  );
+}
+
+// export default MainStackNavigator;
+// export default TabNavigator;
 
 //This was helpful:
 //https://heartbeat.fritz.ai/getting-started-with-stack-navigator-using-react-navigation-5-in-react-native-and-expo-apps-4c516becaee1
