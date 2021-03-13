76e70e46b69071a70aa5a740d3f70977d80f31b6
Chad Laflamme <cjlaflamme1@plymouth.edu>
2021-03-13 17:47:33 -0500

    swagger installed and dtos updated

--- server/src/journal-entry/dto/create-journalEntry.dto.ts
+++ server/src/journal-entry/dto/create-journalEntry.dto.ts
@@ -1,12 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from "src/user/user.entity";
 
 export class CreateJournalEntryDTO {
+    @ApiProperty()
     _id: string;
+
+    @ApiProperty()
     title: string;
+
+    @ApiProperty()
     date: Date;
+
+    @ApiProperty()
     journalEntry: string;
+
+    @ApiProperty()
     sobrietyMaintained: boolean;
+
+    @ApiProperty()
     vibeCheck: number;
+
+    @ApiProperty()
     currentDayOfSobriety: number;
+
+    @ApiProperty({ type: () => User })
     user: User;
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ascent Sobriety Mobile')
+    .setDescription('API routes and descriptions for development.')
+    .setVersion('1.0')
+    // .addTag('ascent')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
