0a5f484c65aadcf7995697c39bcba983e6e2eb5f
Chad Laflamme <cjlaflamme1@plymouth.edu>
2021-03-14 09:47:56 -0400

    password hashing

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UserService) {}
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.userService.userLogIn(username);
+        if (user && user.password === pass) {
+          const { password, ...result } = user;
+          return result;
+        }
+        return null;
+      }
+}
