94f5bfff72312192d4822b3b8736aed2bc628c78
Chad Laflamme <cjlaflamme1@plymouth.edu>
2021-03-14 13:20:41 -0400

    remove demo authguard

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -21,11 +21,5 @@ export class AppController {
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
   
 }
