140b17a43697c391c69c99546704184ad0a08d41
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-03-15 17:26:25 -0400

    Move Logo image and text into its own component, out of Access Screen

---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+// import AppLoading from 'expo-app-loading';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+
+
+const Logo: React.FC = () => {
+  let [fontsLoaded] = useFonts({
+    'Hoefler': require('../../assets/fonts/Hoefler_Text_Regular.ttf'),
+  });
+
+  // if (!fontsLoaded) {
+  //   return <AppLoading />
+  // } else {
+
+  return (
+    <>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo.png')}
+      />
+      <View style={styles.container}>
+
+        <Text style={styles.title}>ASCENT</Text>
+        <Text style={styles.bodyText}>Sobriety Tool</Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  logo: {
+    height: 170,
+    width: 170
+  },
+  title: {
+    fontSize: 39,
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'Hoefler',
+    marginTop: 27
+  },
+  bodyText: {
+    color: '#cfeed5',
+    fontFamily: 'Helvetica',
+    fontSize: 20,
+    marginBottom: 70
+  },
+});
+
+export default Logo;
--- client/src/screens/AccessScreen.tsx
+++ client/src/screens/AccessScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import AppLoading from 'expo-app-loading';
+//import AppLoading from 'expo-app-loading';
 import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
@@ -8,6 +8,8 @@ import { MaterialIcons } from '@expo/vector-icons';
 import Colors from '../constants/Colors.json';
 import Canvas from '../components/Canvas';
 import Card from '../components/Card';
+import Logo from '../components/Logo';
+
 import { RootNavigatorParamsList, TabNavigatorParamsList } from '../types'
 
 interface AccessScreenProps {
@@ -15,79 +17,43 @@ interface AccessScreenProps {
 }
 
 const AccessScreen: React.FC<AccessScreenProps> = ({ navigation }) => {
-  //const { navigation } = props;
-  let [fontsLoaded] = useFonts({
-    'Hoefler': require('../../assets/fonts/Hoefler_Text_Regular.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />
-  } else {
-    return (
-      <Canvas>
-        <Image
-          style={styles.logo}
-          source={require('../../assets/logo.png')}
+  return (
+    <Canvas>
+      <Logo />
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => navigation.navigate('SignIn')}
+        >
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => navigation.navigate('Breathe')}
+        >
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+        <MaterialIcons
+          name="info"
+          style={styles.icon}
+          size={24}
+          color="white"
+          onPress={() => navigation.navigate('About')}
         />
-        <View style={styles.container}>
-
-          <Text style={styles.title}>ASCENT</Text>
-          <Text style={styles.bodyText}>Sobriety Tool</Text>
-
-          <StatusBar style="auto" />
-
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={() => navigation.navigate('Breathe')}
-          >
-            <Text style={styles.buttonText}>Sign In</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={() => navigation.navigate('Breathe')}
-          >
-            <Text style={styles.buttonText}>Sign Up</Text>
-          </TouchableOpacity>
-          <MaterialIcons
-            name="info"
-            style={styles.icon}
-            size={24}
-            color="white"
-            onPress={() => navigation.navigate('About')}
-          />
-        </View>
-      </Canvas>
-    )
-  }
-
+      </View>
+    </Canvas>
+  )
 };
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
   },
-  logo: {
-    height: 170,
-    width: 170
-  },
-  title: {
-    fontSize: 39,
-    fontWeight: 'bold',
-    color: 'white',
-    fontFamily: 'Hoefler',
-    marginTop: 27
-  },
-  bodyText: {
-    color: '#cfeed5',
-    fontFamily: 'Helvetica',
-    fontSize: 20,
-    marginBottom: 70
-  },
-  screen: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#3A5775'
-  },
+  // screen: {
+  //   fontSize: 30,
+  //   fontWeight: 'bold',
+  //   color: '#3A5775'
+  // },
   buttonContainer: {
     alignItems: 'center',
     backgroundColor: Colors.primaryBG,
