7d3c83bd2a69a5a3443406f06143571de2dc0fc7
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-03-18 19:47:09 -0400

    For Logo component, check to see if font is loaded before returning it; if not, return null.

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-// import AppLoading from 'expo-app-loading';
+//import AppLoading from 'expo-app-loading';
 import { Text, View, StyleSheet, Image } from 'react-native';
 import { useFonts } from 'expo-font';
 
 
 const Logo: React.FC = () => {
+  // loading fonts is Async
   let [fontsLoaded] = useFonts({
     'Hoefler': require('../../assets/fonts/Hoefler_Text_Regular.ttf'),
   });
 
-  // if (!fontsLoaded) {
-  //   return <AppLoading />
-  // } else {
+  // if the font hasn't loaded yet, return null, otherwise return the component
+  if (!fontsLoaded) {
+    // return <AppLoading />
+    return null;
+  } else {
+    return (
+      <>
+        <Image
+          style={styles.logo}
+          source={require('../../assets/logo.png')}
+        />
+        <View style={styles.container}>
 
-  return (
-    <>
-      <Image
-        style={styles.logo}
-        source={require('../../assets/logo.png')}
-      />
-      <View style={styles.container}>
+          <Text style={styles.title}>ASCENT</Text>
+          <Text style={styles.bodyText}>Sobriety Tool</Text>
 
-        <Text style={styles.title}>ASCENT</Text>
-        <Text style={styles.bodyText}>Sobriety Tool</Text>
-
-        <StatusBar style="auto" />
-      </View>
-    </>
-  )
+          <StatusBar style="auto" />
+        </View>
+      </>
+    )
+  }
 };
 
 const styles = StyleSheet.create({
