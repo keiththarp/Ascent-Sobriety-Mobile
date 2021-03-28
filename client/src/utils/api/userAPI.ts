e5d10ed6d2768d9a7710dbbe4984f812425bb568
Chad Laflamme <cjlaflamme1@plymouth.edu>
2021-03-28 16:40:53 -0400

    cleaning up user API

--- client/src/utils/api/userAPI.ts
+++ client/src/utils/api/userAPI.ts
@@ -2,15 +2,6 @@ import axios from 'axios';
 import { IUser } from "../../interfaces/interfaces";
 import { API_URL } from "@env";
 
-
-const userAPI = () => {
-  fetch(`${process.env.API_URL}/users`)
-    .then(response => {
-      console.log(response);
-
-    })
-}
-
 export const signUpUserAPI = (user: IUser) => {
     return axios.post(`${API_URL}/auth/signup`,user);
 
