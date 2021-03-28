a2a7a8262baef3744799ee0f245b06aa896ef6e8
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-03-28 16:23:57 -0400

    Get environment variables working properly

--- client/src/utils/api/userAPI.ts
+++ client/src/utils/api/userAPI.ts
@@ -1,17 +1,18 @@
+import { API_URL } from '@env';
 import axios from 'axios';
 import { IUser } from "../../interfaces/interfaces";
-import { API_URL } from "@env";
 
-const userAPI = () => {
-  fetch(`${process.env.API_URL}/users`)
-    .then(response => {
-      console.log(response);
+// const userAPI = () => {
+//   fetch(`${process.env.API_URL}/users`)
+//     .then(response => {
+//       console.log(response);
 
-    })
-}
+//     })
+// }
 
 export const signUpUserAPI = (user: IUser) => {
-    return axios.post(`${API_URL}/auth/signup`,user);
-
+  //console.log(API_URL);
+  return axios.post(`${API_URL}/auth/signup`, user);
+  //return axios.post ("http://ascentsobrietymobile-env.eba-rw6cfpmf.us-east-2.elasticbeanstalk.com/auth/signup", user);
 }
 
