560fd8c41ad20039b09fd687c902c7ff72b0f960
Chad Laflamme <cjlaflamme1@plymouth.edu>
2021-04-03 17:27:44 -0400

    update user api

--- client/src/utils/api/userAPI.ts
+++ client/src/utils/api/userAPI.ts
@@ -4,10 +4,17 @@ import { API_URL } from "@env";
 
 export const signUpUserAPI = (user: IUser) => {
     return axios.post(`${API_URL}/auth/signup`,user);
-
-}
+};
 
 export const signInUserAPI = (email: string, password: string) => {
   return axios.post(`${API_URL}/auth/login`, { email, password});
-}
+};
+
+export const updateUserAPI = (user: IUser, JWT_TOKEN: string) => {
+  return axios.post(`${API_URL}/api/user`, user, {
+    headers: {
+      'Authorization': `Bearer ${JWT_TOKEN}`
+    }
+  });
+};
 
